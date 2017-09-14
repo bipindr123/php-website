fc207ab336010bac42056d4284fa7cd9eab1edb1
bipindr123 <bipindr123@gmail.com>
2017-09-14 20:14:09 +0530

    inti

---
+++ untitled2/view.py
@@ -0,0 +1,17 @@
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render, get_object_or_404
+
+
+# Create your views here.
+
+
+def index(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'index.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request,'index.html')
